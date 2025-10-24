e9be9f1e440e3fc4b4d89fcb94244bd52f68e470
deyciandreacamargoramos5-creator <dcamargor@unsa.edu.pe>
2025-10-24 18:37:59 -0500

    Subiendo el segundo commit: Ejercicio 2

--- Ejercicio1.js
+++ Ejercicio1.js
@@ -1,11 +1,12 @@
 function CalcularPromedio(){
     let suma = 0;
-    let cantidadNotas = 0;
+    let cantidadNotas = 5;
     for (let i = 1; i <= cantidadNotas; i++){
         let nota;
         do{
             nota = parseFloat(prompt("Ingrese la nota"));
             if(isNaN(nota) || nota < 0 || nota < 20){
+                console.log("Dato incorrecto");
             }
         }while(isNaN(nota) || nota < 0 || nota > 20);
         suma += nota;
---
+++ Ejercicio2.js
@@ -0,0 +1,16 @@
+function Edad(){
+    let edad = parseInt(prompt("Ingrese una edad"));
+    let categoria;
+    if(edad < 12){
+        categoria ="niño";
+    }else if (edad < 18){
+        categoria ="Adolescente";
+    }else if (edad < 60){
+        categoria ="Adulto";
+    }else{
+        categoria = "Adulto mayor"
+    }
+    return categoria;
+}
+let resultado = Edad();
+console.log(resultado);
