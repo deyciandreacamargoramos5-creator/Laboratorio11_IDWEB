42ee8c755469c3ed3e0888f904fdf033ab8b2a95
deyciandreacamargoramos5-creator <dcamargor@unsa.edu.pe>
2025-10-24 18:45:38 -0500

    Subiendo el tercer commit: Ejercicio 3

---
+++ Ejercicio3.js
@@ -0,0 +1,19 @@
+function contarParesImpares() {
+let pares = 0;
+    let impares = 0;
+    
+    for(let i = 1; i <= 10; i++) {
+        let numero = parseInt(prompt("Ingrese el número"));
+        
+        if(numero % 2 === 0) {
+            pares++;
+        } else {
+            impares++;
+        }
+    }
+    console.log("Números pares: " + pares);
+    console.log("Números impares: " + impares);
+    
+    return { pares, impares };
+}
+contarParesImpares();
